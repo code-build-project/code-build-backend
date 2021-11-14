ea6e0d7ca4d4cdb7366a79ff809979a624d551d8
CodeBuild <code-build@yandex.ru>
2021-11-15 00:55:51 +0400

    update

--- src/models/articles.js
+++ src/models/articles.js
@@ -2,11 +2,12 @@ export class Article {
   constructor(article) {
     this._id = article._id;
     this.title = article.title;
-    this.date = article.date; 
+    this.date = article.date;
     this.time = article.time;
     this.views = article.views;
     this.tags = article.tags;
     this.likes = article.likes;
     this.image = article.image;
+    this.content = article.content;
   }
 }
